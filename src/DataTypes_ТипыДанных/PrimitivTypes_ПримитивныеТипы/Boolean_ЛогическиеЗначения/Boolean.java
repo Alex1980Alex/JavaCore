7b85f1957061753035bb06be9f014a799e0531a5
Alexey Terletsky <79525997152@ya.ru>
2023-09-08 00:47:25 +0300

    Boolean в Java

---
+++ src/DataTypes_ТипыДанных/PrimitivTypes_ПримитивныеТипы/Boolean_ЛогическиеЗначения/Boolean.java
@@ -0,0 +1,15 @@
+package DataTypes_ТипыДанных.PrimitivTypes_ПримитивныеТипы.Boolean_ЛогическиеЗначения;
+
+public class Boolean {
+    public static void main(String[] args) {
+                // Boolean в Java — это логический тип данных.
+                // Переменная типа boolean может принимать всего два значения
+                // — это правда или ложь — true & false.
+                boolean BoolTrue = true;
+                System.out.println(BoolTrue);
+                boolean BoolFalse = false;
+                System.out.println(BoolFalse);
+
+    }
+    }
+
