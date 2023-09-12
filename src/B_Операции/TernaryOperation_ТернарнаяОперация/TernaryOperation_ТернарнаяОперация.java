d65366e8fb88ef45e33dc3a1d39f653923d91d53
Alexey Terletsky <79525997152@ya.ru>
2023-09-12 23:02:17 +0300

    Ternary operation

---
+++ src/B_Операции/TernaryOperation_ТернарнаяОперация/TernaryOperation_ТернарнаяОперация.java
@@ -0,0 +1,12 @@
+package B_Операции.TernaryOperation_ТернарнаяОперация;
+
+public class TernaryOperation_ТернарнаяОперация {
+    public static void main(String[] args) {
+        //Тернарная операция
+        System.out.println("Тернарная операция");
+        int число = 10;
+        String результат = (число > 5) ? "Число больше 5" : "Число не больше 5";
+
+        System.out.println(результат); // Выводит "Число больше 5", так как число больше 5
+    }
+}
